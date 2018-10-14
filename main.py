8ef6efc6993979c707e60c6b52fcc32dd1dd4617
Ali Shakeri <alishakeri88@gamail.com>
2018-10-14 13:28:24 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+def print_dict(path, ind):
+	indent =  '\t' * int(ind)
+	for child in os.listdir(path):
+		child_path = os.path.join(path, child)
+		if os.path.isdir(child_path):
+			print_dict(child_path, ind + 1)
+		else:
+			print indent + "+" + child_path
+
+print_dict('/home/ali/Desktop', 0)
